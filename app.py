c801d3f6bafd2a986718ad03d1dec8ef72fff6d1
Sébastien Garcia <garc.sebastien@gmail.com>
2024-07-08 08:48:11 +0200

    Modification du fichier app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify, render_template, request
 import pandas as pd
 import pickle
+import logging
 
 app = Flask(__name__)
 
@@ -18,32 +19,32 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Obtenir l'identifiant du client à analyser
-    client_id = int(request.form['client_id'])
+    try:
+        # Obtenir l'identifiant du client à analyser
+        client_id = int(request.form['client_id'])
 
-    # Vérifier si l'identifiant du client est présent dans le dataframe
-    if client_id in df['SK_ID_CURR'].values:
-        # Sélectionner les données client
-        features_client = df.loc[df['SK_ID_CURR'] == client_id]
-        # Suppression des colonnes SK_ID_CURR et TARGET
-        features_client = features_client.drop(columns=['SK_ID_CURR', 'TARGET'])
-    
-        # Faire la prédiction
-        prob = model.predict_proba(features_client)[:, 1]
-        pred = (prob >= 0.52).astype(int)
+        # Vérifier si l'identifiant du client est présent dans le dataframe
+        if client_id in df['SK_ID_CURR'].values:
+            # Sélectionner les données client
+            features_client = df.loc[df['SK_ID_CURR'] == client_id]
+            # Suppression des colonnes SK_ID_CURR et TARGET
+            features_client = features_client.drop(columns=['SK_ID_CURR', 'TARGET'])
 
-        response = {
-            'proba': prob.tolist(),
-            'prediction': pred.tolist()
-        }
-    
-    else:
-        # Afficher un message d'erreur si l'identifiant du client n'est pas trouvé
-        response = {
-            'erreur': 'Identifiant du client incorrect'
-        }
-    
-    return jsonify(response)
+            # Faire la prédiction
+            prob = model.predict_proba(features_client)[:, 1]
+            pred = (prob >= 0.52).astype(int)
+
+            response = {
+                'proba': prob.tolist(),
+                'prediction': pred.tolist()
+            }
+        else:
+            response = {'erreur': 'Identifiant du client incorrect'}
+
+        return jsonify(response)
+    except Exception as e:
+        logging.error(f"Error during prediction: {str(e)}")
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(port=8000)
