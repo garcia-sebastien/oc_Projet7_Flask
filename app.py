390c3c4fc1424cf2c0784e9423df4248223dd763
Sébastien Garcia <garc.sebastien@gmail.com>
2024-07-08 08:53:40 +0200

    Modification du fichier requirements.txt

--- app.py
+++ app.py
@@ -43,8 +43,7 @@ def predict():
 
         return jsonify(response)
     except Exception as e:
-        logging.error(f"Error during prediction: {str(e)}")
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(port=8000)
